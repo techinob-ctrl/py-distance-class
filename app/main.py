cf2261233bf51494063209ec610b6bf18fcc9ea0
Techin Rungvitayatiwat <techino.b@gmail.com>
2025-12-26 20:07:10 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,69 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        other_km = self._extract_km(other)
+        if other_km is NotImplemented:
+            return NotImplemented
+        return Distance(self.km + other_km)
+
+    def __iadd__(self, other):
+        other_km = self._extract_km(other)
+        if other_km is NotImplemented:
+            return NotImplemented
+        self.km += other_km
+        return self
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
+        return NotImplemented
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
+        return NotImplemented
+
+    def __lt__(self, other):
+        other_km = self._extract_km(other)
+        if other_km is NotImplemented:
+            return NotImplemented
+        return self.km < other_km
+
+    def __gt__(self, other):
+        other_km = self._extract_km(other)
+        if other_km is NotImplemented:
+            return NotImplemented
+        return self.km > other_km
+
+    def __eq__(self, other):
+        other_km = self._extract_km(other)
+        if other_km is NotImplemented:
+            return NotImplemented
+        return self.km == other_km
+
+    def __le__(self, other):
+        other_km = self._extract_km(other)
+        if other_km is NotImplemented:
+            return NotImplemented
+        return self.km <= other_km
+
+    def __ge__(self, other):
+        other_km = self._extract_km(other)
+        if other_km is NotImplemented:
+            return NotImplemented
+        return self.km >= other_km
+
+    def _extract_km(self, other):
+        if isinstance(other, Distance):
+            return other.km
+        if isinstance(other, (int, float)):
+            return other
+        return NotImplemented
